f098d8e7c14cc35d5071c704033678cd50537f40
obed02 <74347536+obed02@users.noreply.github.com>
2020-11-16 09:11:48 +0000

    Update __main__.py

--- haruka/__main__.py
+++ haruka/__main__.py
@@ -22,20 +22,12 @@ from haruka.modules.connection import connected
 
 PM_START = """Hello {}, my name is {}!
 
-You know how hard it is sometimes to manage group so here is the solution for you
-
-I'm group manager bot and Anti-spam for RR Players🃏
-
+Hey there! My name is Cindy- I'm here to help you manage your groups! Hit /help to find out more about how to use me to my full potential.
 Activate Anti-spam protection in your BM by this command : 
 /antispam on 
 
-Made from the source code of [Aditya 🇮🇳](t.me/xditya)
+Made By [X_PRESIDENT1 🇬🇭](https://t.me/X_PRESIDENT1)
 
-Special thanks to [this person](t.me/denver02) for helping me.
-
-Deploy a bot like me [now](www.github.com/xditya/GroupManager)
-
-Watch [video tutorial](https://www.youtube.com/watch?v=gXXFpTAk6Vo&feature=youtu.be) on deploying me.
 
 Click /help or Help button below to find out more about how to use me to my full potential.
 
